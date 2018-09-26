6c4aa2cb9b1ceb728de7acb003e2773be09a1161
mxl <2194596537@qq.com>
2018-09-26 22:46:19 +0800

    重置功能完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -12,6 +12,9 @@ $(function(){
                     notEmpty:{
                         message:"用户名不能为空"
                     },
+                    callback:{
+                        message:"用户名错误"
+                      }
                   }
               },
               password:{
@@ -20,19 +23,43 @@ $(function(){
                           message:"密码不能为空"
                       },
                       stringLength:{
-                        min:2,
-                        max:6,
-                        message:"用户名长度必须2~6位"
-                    }
+                        min:6,
+                        max:12,
+                        message:"用户名长度必须6~12位"
+                    },
+                    callback:{
+                        message:"密码错误"
+                      }
                   }
               },
-              callback:{
-                message:"密码错误"
-              }
           }
     })
     // 表单校验成功,注册表单校验成功的事件，阻止默认提 交，使用ajax提交
-    $.ajax({
-        
+    $("#form").on('success.form.bv',function(e){
+        e.preventDefault();
+        $.ajax({
+            type:"post",
+            url:"/employee/employeeLogin",
+            data:$("#form").serialize(),
+            dataType:"json",
+            success:function(info){
+                console.log(info)
+               if(info.success){
+                   location.href="index.html";
+               }
+               else if(info.error==1000){
+                   message:"用户名不存在!"
+                   $(form).data('bootstrapValidator').updateStatus('username','INVALID','callback')
+               }
+               else if(info.error==1001){
+                message:"密码错误"
+                $(form).data('bootstrapValidator').updateStatus('password','INVALID','callback')
+                }
+            }
+        })
+    })
+    $('[type="reset"]').on('click',function(){
+        console.log($(form).data('bootstrapValidator'));
+        $(form).data('bootstrapValidator').resetForm();
     })
 })
