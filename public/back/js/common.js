1d2dc1ffb924545aa164b37ce8ac35aea57c2e6e
mxl <2194596537@qq.com>
2018-09-28 17:31:06 +0800

    模态框以及退出功能完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -6,3 +6,26 @@ $(document).ajaxStop(function(){
         NProgress.done()
     },500)
 })
+//实现公共部分的分类页切换功能
+$(function(){
+    $('.nav .cate').on('click',function(){
+        $('.nav .child').slideToggle();
+    })
+})
+// 模态框
+$('.loginOut').on('click', function () {
+    $('#loginOut').modal('show')
+  })
+$('.btn-primary').on('click',function(){
+    $.ajax({
+        type: "get",
+        url: "/employee/employeeLogout",
+        dataType: "json",
+        success: function( info ) {
+          if ( info.success ) {
+            // 退出成功, 跳转到登陆页
+            location.href = "login.html";
+          }
+        }
+      })
+})
